57ecb1efe9618f702287b611028f38e254a03697
thinkdigitalrepair <thinkdigitalrepair@gmail.com>
2018-01-09 19:04:04 -0800

    Cleanup

--- livekh.py
+++ livekh.py
@@ -3,28 +3,19 @@ import regex as r
 import argparse
 
 parser = argparse.ArgumentParser(
-    description="This is a program to download meeting recordings from www.livekingdomhall.com")
+    description="This is a program to download meeting recordings from www.livekingdomhall.com. " \
+                "If you don't know your congregation id, enter 0 for the id and use the --lookup command.")
 parser.add_argument("congregation_id", action="store", help="your congregation id")
 parser.add_argument("--mm", action="store_true", default=False, help="download the recording from midweek meeting")
 parser.add_argument("--wm", action="store_true", default=False, help="download the recording from weekend meeting")
+parser.add_argument("--lookup", action="store",
+                    help="follow this option with the url of the congregation you want to lookup")
 args = parser.parse_args()
 
-congregation_id = args.congregation_id
 
-"""session = requests.Session()
-#session.post("https://www.livekingdomhall.com/kingdomhall/authenticate", data={"list_pass", "1522"})
-session_get = session.get(
-    "https://www.livekingdomhall.com/kingdomhall/listen/West%20Desert%20Hot%20Springs,%20USA/ODY4Nw==",
-    auth=('1522', ''))
-request = requests.get(
-    "https://www.livekingdomhall.com/kingdomhall/listen/West%20Desert%20Hot%20Springs,%20USA/ODY4Nw==",
-    auth=('1522', ''))
-"""
-
-
-# currently, as of 1-7-2018, livekingdomhall doesn't use any authentication behind the scenes. This allows us to
+# As of 1-7-2018, livekingdomhall doesn't use any authentication behind the scenes. This allows us to
 # access our files without having to login. This may change. Please do not abuse this fact.
-def get_cong_id(url):
+def get_congregation_id(url):
     """
     grab congregation_id from webpage and return it
     :param url: the url of the kingdom hall's page.
@@ -32,33 +23,34 @@ def get_cong_id(url):
     """
     text = requests.get(url).text
     result = r.findall('\<input type\=\"hidden\" name\=\"congregation_id\" value\=\"(\d+)\"/>', text)
-    if len(result) == 0:
-        return False
-    else:
-        return result[0]
+
+    return False if len(result) == 0 else result[0]
 
 
-def download(congregation_id: int, filename) -> object:
+def download(c_id: int, fn) -> object:
     """
+    :param fn: the name to save the file under
     :return:
-    :param congregation_id: The id for your kingdom hall.
+    :param c_id: the id for your congregation.
     :return: request object
     """
-    assert congregation_id != 0
+    assert c_id != 0
 
-    download_url = "http://162.244.81.220/audio_recordings/" + filename
+    download_url = "http://162.244.81.220/audio_recordings/" + fn
     mp3 = requests.get(download_url)
-    with open(filename, 'wb') as fd:
+    with open(fn, 'wb') as fd:
         for chunk in mp3.iter_content(chunk_size=128):
             fd.write(chunk)
     return mp3
 
 
-id = get_cong_id("https://www.livekingdomhall.com/kingdomhall/NzY1NA==/East+Desert+Hot+Springs%2C+USA")
-if args.mm:
-    filename = "record_{0}_{1}.mp3".format(congregation_id, "MM")
-    download(args.congregation_id, filename=filename)
-if args.wm:
-    filename = "record_{0}_{1}.mp3".format(congregation_id, "WM")
-    download(args.congregation_id, filename=filename)
-pass
+if __name__ == '__main__':
+    if args.lookup:
+        congregation_id = get_congregation_id(args.lookup)
+
+    if args.mm:  # if midweek meeting download flag is set
+        filename = "record_{0}_{1}.mp3".format(congregation_id, "MM")
+        download(args.congregation_id, fn=filename)
+    if args.wm:  # if weekend meeting download flag is set
+        filename = "record_{0}_{1}.mp3".format(congregation_id, "WM")
+        download(args.congregation_id, fn=filename)
